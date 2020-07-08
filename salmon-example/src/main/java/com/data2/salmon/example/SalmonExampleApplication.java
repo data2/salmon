a0344244704ceec7d0aa10da6207ad3d41759bdd
data2 <muskteerathos@gmail.com>
2020-07-08 20:47:36 +0800

    测试

--- salmon-example/src/main/java/com/data2/salmon/example/SalmonExampleApplication.java
+++ salmon-example/src/main/java/com/data2/salmon/example/SalmonExampleApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author leewow
@@ -18,33 +19,36 @@ import java.util.Collections;
 @RestController
 public class SalmonExampleApplication {
 
-//    @Autowired(required = false)
-//    @Mapper(file = "mapper1", database = DataBase.PARTITION, name = "salman_partition_dao")
-//    public Salmon salmon;
+    @Autowired(required = false)
+    @Mapper(file = "mapper", database = DataBase.JDBC, name = "salman_jdbc_dao")
+    public Salmon salmon;
 
     @Autowired(required = false)
-    @Mapper(file = "mapper1", database = DataBase.JDBC, name = "salman_jdbc_dao")
+    @Mapper(file = "mapper", database = DataBase.PARTITION, name = "salman_partition_dao")
     public Salmon salmon2;
 
     public static void main(String[] args) {
         SpringApplication.run(SalmonExampleApplication.class, args);
-
     }
 
-    @RequestMapping("/")
-    public void test() {
-        System.out.println("test....");
-        Object obj2 = salmon2.select("test")
-                .param(Collections.singletonMap("articleID", "243992049376706560")).execute();
-        System.out.println(obj2);
-    }
-
-    @RequestMapping("/test2")
-    public void test2() {
-        System.out.println("test2....");
-        Object obj = salmon2.select("easy").execute();
+    @RequestMapping("/easy")
+    public void testEasy() {
+        Object obj = salmon.select("easy").execute();
         System.out.println(obj);
     }
+
+    @RequestMapping("/single")
+    public Object testSingle() {
+        Map map = (Map) salmon.select("single").param(Collections.singletonMap("articleID", "243992049376706560")).execute();
+        return map;
+    }
+
+    @RequestMapping("/single2")
+    public Object testSingle2() {
+        Map map = (Map) salmon.select("single2").param("243992049376706560").execute();
+        return map;
+    }
+
 
 
 }
