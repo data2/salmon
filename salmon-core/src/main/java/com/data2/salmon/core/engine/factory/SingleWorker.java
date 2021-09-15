40865429989f91b5d479ac73a8d8cc14301471df
data2 <muskteerathos@gmail.com>
2021-09-15 19:25:04 +0800

    支持事物

--- salmon-core/src/main/java/com/data2/salmon/core/engine/factory/SingleWorker.java
+++ salmon-core/src/main/java/com/data2/salmon/core/engine/factory/SingleWorker.java
@@ -1,10 +1,13 @@
 package com.data2.salmon.core.engine.factory;
 
 import com.data2.salmon.core.engine.config.ParseConfig;
+import com.data2.salmon.core.engine.except.SalmonException;
 import com.data2.salmon.core.engine.inter.QuickService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
 
 /**
  * @author data2
@@ -18,24 +21,41 @@ public class SingleWorker extends QuickService {
     private ParseConfig parseConfig;
 
     @Override
-    public Object execute(Object object) {
-        return handle(object);
+    public Object execute(Object object) throws SalmonException, SQLException {
+        param(object);
+        return handle();
     }
 
     @Override
-    public Object execute() {
-        return handle(null);
+    public Object execute() throws SalmonException, SQLException {
+        return execute(null);
     }
 
-    public Object handle(Object object) {
+    private Object handle() throws SalmonException, SQLException {
         try {
-            return buildFactory.build(database, currSql.setSql(parseConfig.parse(file, currSql)), object);
+            buildFactory.build(database, currSql.setSql(parseConfig.parse(file, currSql)), currParams);
+            if (!currSql.isTrans()) {
+                buildFactory.giveSource(currSql);
+            } else {
+                if (sqlQueue.get().size() == 1) {
+                    buildFactory.giveSource(currSql);
+                } else {
+                    if (currSql.getLooker().toString().equals(sqlQueue.get().get(0).getLooker().toString())) {
+                        buildFactory.copySource(sqlQueue.get().get(0), currSql);
+                    } else {
+                        buildFactory.giveSource(currSql);
+                    }
+                }
+            }
+            return buildFactory.run(currSql, currParams);
         } catch (Exception e) {
             e.printStackTrace();
             log.error("execute err,{}", e.getMessage());
-            exception = true;
-            return null;
+            currSql.setExcept(true);
+            commitTrans();
+            throw e;
         }
     }
+
 
 }
