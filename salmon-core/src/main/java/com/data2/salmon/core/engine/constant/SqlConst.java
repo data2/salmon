4ee8162372d21ff5044a7a208442bcd1e9009504
data2 <muskteerathos@gmail.com>
2021-09-15 19:24:02 +0800

    add const

---
+++ salmon-core/src/main/java/com/data2/salmon/core/engine/constant/SqlConst.java
@@ -0,0 +1,5 @@
+package com.data2.salmon.core.engine.constant;
+
+public class SqlConst {
+
+}
---
+++ salmon-core/src/main/java/com/data2/salmon/core/engine/strategy/StrategyConst.java
@@ -0,0 +1,7 @@
+package com.data2.salmon.core.engine.strategy;
+
+
+public class StrategyConst {
+
+    public static final int PARAM_LEN_THREE = 3;
+}
