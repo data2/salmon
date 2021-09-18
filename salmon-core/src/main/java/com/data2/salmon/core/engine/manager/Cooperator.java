48a90f3bf1d6975124fb8ec9c4f202d4dde01f53
data2 <muskteerathos@gmail.com>
2021-09-18 23:11:33 +0800

    cancel parmaList

--- salmon-core/src/main/java/com/data2/salmon/core/engine/manager/Cooperator.java
+++ salmon-core/src/main/java/com/data2/salmon/core/engine/manager/Cooperator.java
@@ -4,31 +4,23 @@ import com.data2.salmon.core.engine.domain.ExecuteSql;
 import com.google.common.collect.Lists;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class Cooperator {
-    private ThreadLocal<List<ExecuteSql>> sqlQueue = new ThreadLocal<>();
-    private ThreadLocal<List<Object>> paramQueue = new ThreadLocal<>();
+    private ThreadLocal<LinkedList<ExecuteSql>> sqlQueue = new ThreadLocal<>();
 
     public void clear() {
         sqlQueue.remove();
-        paramQueue.remove();
-
     }
 
     public void init() {
-        sqlQueue.set(Lists.newArrayList());
-        paramQueue.set(Lists.newArrayList());
+        sqlQueue.set(Lists.newLinkedList());
     }
 
-    public List<ExecuteSql> sqls() {
+    public LinkedList<ExecuteSql> sqls() {
         return sqlQueue.get();
     }
-
-    public List<Object> params() {
-        return paramQueue.get();
-    }
-
 
 }
