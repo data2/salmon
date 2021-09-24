fe3ef8ccb2944aa65e2b205ebdfe8906a13da522
data2 <muskteerathos@gmail.com>
2021-09-24 21:12:44 +0800

    judge is null ,before close conn

--- salmon-core/src/main/java/com/data2/salmon/core/engine/manager/Cooperator.java
+++ salmon-core/src/main/java/com/data2/salmon/core/engine/manager/Cooperator.java
@@ -1,17 +1,30 @@
 package com.data2.salmon.core.engine.manager;
 
 import com.data2.salmon.core.engine.domain.ExecuteSql;
+import com.data2.salmon.core.engine.inter.CloseSource;
 import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Objects;
 
+/**
+ * @author data2
+ * @date
+ */
 @Component
-public class Cooperator {
+@Slf4j
+public class Cooperator extends CloseSource {
     private ThreadLocal<LinkedList<ExecuteSql>> sqlQueue = new ThreadLocal<>();
 
     public void clear() {
+        if (Objects.isNull(sqlQueue.get())){
+            return;
+        }
+        sqlQueue.get().forEach(sql -> {
+            close(sql);
+        });
         sqlQueue.remove();
     }
 
